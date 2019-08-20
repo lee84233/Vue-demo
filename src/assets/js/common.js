191292ba17d16efa64457615b6f0a186a81b2b44
01427708 <licheng.dts@haier.com>
2019-08-20 15:08:39 +0800

    update

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -9,7 +9,7 @@
 /**
  * 验证是否为手机号
  * @param  {number, String} phone：要验证手机号
- * @return {Boolean} [是否为手机号]
+ * @returns {Boolean} [是否为手机号]
  */
 export function isPhone(phone) {
   let reg = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
