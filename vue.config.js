419a7628db6fafd40e11657dfcf000c87780589c
lee84233 <842337932@qq.com>
2019-10-17 15:20:51 +0800

    :smile: 配置文件

--- vue.config.js
+++ vue.config.js
@@ -9,16 +9,23 @@
 module.exports = {
   // 部署应用包时的基本 URL
   publicPath: '/',
+
+  // 打包生成构建文件的目录，默认值：dist
+  // outputDir: 'dist',
+
   // 放置生成的静态资源的(相对于 outputDir 的) 目录
   assetsDir: 'assets',
+
   // 生产环境的 source map
   productionSourceMap: false,
+
   // 配置 webpack，会合并到最终的配置中
   // configureWebpack: {
   //   plugins: [
   //     new webpack.ProvidePlugin({})
   //   ]
   // },
+
   devServer: {
     host: '0.0.0.0',
     port: 8080,
